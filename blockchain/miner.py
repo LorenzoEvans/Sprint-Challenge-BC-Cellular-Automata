cf254d6d7fa2a1b7102c51f22e789b575c7c1515
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-08-17 12:42:12 -0400

    Working on blockchain functions.

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -30,13 +30,15 @@ def proof_of_work(last_proof):
 
 
 def valid_proof(last_hash, proof):
+ 
     """
     Validates the Proof:  Multi-ouroborus:  Do the last six characters of
     the last hash match the first six characters of the proof?
 
     IE:  last_hash: ...999123456, new hash 123456888...
     """
-
+    last_six = last_hash[-6:]
+    first_proof = proof[0:6]
     # TODO: Your code here!
     pass
 
